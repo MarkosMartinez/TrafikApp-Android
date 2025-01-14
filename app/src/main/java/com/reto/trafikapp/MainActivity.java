acbf3ee2c5a82e6f0e16607bbaa8b4898812796b
MarkosMartinez <ikbdk@plaiaundi.net>
2025-01-14 20:01:22 +0100

    Avanzando con el login y mapa

--- app/src/main/java/com/reto/trafikapp/MainActivity.java
+++ app/src/main/java/com/reto/trafikapp/MainActivity.java
@@ -1,72 +1,71 @@
 package com.reto.trafikapp;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.EditText;
-import android.util.Log;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-    Button buttonLogin;
-    EditText editTextEmail;
-    EditText editTextPass;
-    LlamadasAPI llamadasAPI = new LlamadasAPI();
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private MapView mapView;
+
+    private static final String MAPVIEW_BUNDLE_KEY = "MapViewBundleKey";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-
-        SharedPreferences sharedPreferences = getSharedPreferences("config", MODE_PRIVATE);
-        boolean isLoggedIn = sharedPreferences.getBoolean("estaLogueado", false);
-
-        if (isLoggedIn) {
-            Log.d("MainActivity", "Usuario logueado");
-        } else {
-            Log.d("MainActivity", "Usuario no logueado");
-            setContentView(R.layout.activity_main);
-
-            buttonLogin = findViewById(R.id.buttonLogin);
-            editTextEmail = findViewById(R.id.editTextEmail);
-            editTextPass = findViewById(R.id.editTextPass);
-
-            ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-                Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-                v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-                return insets;
-            });
-
-            buttonLogin.setOnClickListener(v -> {
-                llamadasAPI.login(editTextEmail.getText().toString(), editTextPass.getText().toString(), new LlamadasAPI.LoginCallback() {
-                    @Override
-                    public void onSuccess(boolean isSuccess) {
-                        runOnUiThread(() -> {
-                            if (isSuccess) {
-                                SharedPreferences.Editor editor = sharedPreferences.edit();
-                                editor.putBoolean("estaLogueado", true);
-                                editor.apply();
-                                Log.d("MainActivity", "Usuario logueado");
-                            } else {
-                                Log.d("MainActivity", "Credenciales no validas");
-                            }
-                        });
-                    }
-
-                    @Override
-                    public void onFailure() {
-                        runOnUiThread(() -> {
-                            Log.d("MainActivity", "Credenciales no validas");
-                        });
-                    }
-                });
-            });
+        setContentView(R.layout.activity_main); // Llamarlo antes de los findViewById
+        mapView = findViewById(R.id.mapView);
+        Bundle mapViewBundle = null;
+        if (savedInstanceState != null) {
+            mapViewBundle = savedInstanceState.getBundle(MAPVIEW_BUNDLE_KEY);
         }
+        mapView.onCreate(mapViewBundle);
+        // Inicia la carga del mapa
+        mapView.getMapAsync(this);
+
+        ImageButton ibtnLogout = findViewById(R.id.imageButtonLogout);
+
+        ibtnLogout.setOnClickListener(v -> {
+            SharedPreferences sharedPreferences = getSharedPreferences("config", MODE_PRIVATE);
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putBoolean("estaLogueado", false);
+            editor.apply();
+            Toast.makeText(MainActivity.this, R.string.activity_main_toast_logout, Toast.LENGTH_SHORT).show();
+            startActivity(new Intent(MainActivity.this, LoginActivity.class));
+            finish();
+        });
+
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+
+        LatLng ejemplo = new LatLng(43.347160, -1.793184);  // Ejemplo: Latitud y Longitud de Sydney
+        googleMap.addMarker(new MarkerOptions().position(ejemplo).title("Marcador en Plaiaundi"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(ejemplo));
+        googleMap.getUiSettings().setMapToolbarEnabled(false); // Ocultar los iconos de maps de abajo a la derecha
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Bundle mapViewBundle = outState.getBundle(MAPVIEW_BUNDLE_KEY);
+        if (mapViewBundle == null) {
+            mapViewBundle = new Bundle();
+            outState.putBundle(MAPVIEW_BUNDLE_KEY, mapViewBundle);
+        }
+        mapView.onSaveInstanceState(mapViewBundle);
     }
 }
