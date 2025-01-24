0b7dc89d9b8f240b44edde0624c0b3f79ef880ea
MarkosMartinez <ikbdk@plaiaundi.net>
2025-01-24 10:59:20 +0100

    Pequeños avances con la vista de la camara

--- app/src/main/java/com/reto/trafikapp/CamaraActivity.java
+++ app/src/main/java/com/reto/trafikapp/CamaraActivity.java
@@ -2,6 +2,7 @@ package com.reto.trafikapp;
 
 import android.os.Bundle;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,17 +13,28 @@ import com.bumptech.glide.Glide;
 
 public class CamaraActivity extends AppCompatActivity implements Serializable {
     ImageView imgCamara;
+    TextView nombreCamara;
+    TextView nombreCarretera;
+    TextView ubicacion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camara);
         imgCamara = findViewById(R.id.imgCamara);
+        nombreCamara = findViewById(R.id.textViewCamara);
+        nombreCarretera = findViewById(R.id.textViewCarretera);
+        ubicacion = findViewById(R.id.textViewUbicacion);
+
         Camara camara = (Camara) getIntent().getSerializableExtra("camara");
         if (camara != null) {
             Glide.with(this)
                     .load(camara.getUrlImage())
                     .into(imgCamara);
+            nombreCamara.setText(camara.getCameraName());
+            nombreCarretera.setText(camara.getRoad());
+            ubicacion.setText(camara.getAddress());
+
         }
 
 
